ad6c779a33ba055dc804c85451e6d01fe2243a13
Carina Cunha <carina.cunha13@gmail.com>
2022-08-16 17:52:45 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testa o componente About', () => {
+  test('Verifica se a página contém um heading h2 com o texto Page requested not', () => {
+    renderWithRouter(<NotFound />);
+
+    const subtitle = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(subtitle).toBeInTheDocument();
+  });
+
+  test('Verifica se a página mostra a imagem com URL específica', () => {
+    renderWithRouter(<NotFound />);
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole('img', { name: /Pikachu crying/ });
+    expect(image.src).toContain(url);
+  });
+});
