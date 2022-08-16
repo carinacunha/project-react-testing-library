d772fc78cc9f54cd2ba09141c26d6e40bf8296d7
Carina Cunha <carina.cunha13@gmail.com>
2022-08-16 17:37:42 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Testa o componente FavoritePokemons', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const textnotFavorite = screen.getByText(/No favorite/i);
+    expect(textnotFavorite).toBeInTheDocument();
+  });
+});
