53e3aee535b1f2ca788f216e7dd8fa48eea3a8ef
Carina Cunha <carina.cunha13@gmail.com>
2022-08-16 16:36:41 -0300

    Correção do requisito 1 e adição do requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
 
+describe('Testa o componente About', () => {
+  test('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const subtitle = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(subtitle).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const firstParagraph = screen.getByText(/This application/i);
+    const secondParagraph = screen.getByText(/One can filter/i);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const PokedexImage = screen.getByRole('img');
+    expect(PokedexImage.src).toContain(url);
+  });
+});
